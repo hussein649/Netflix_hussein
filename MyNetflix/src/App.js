056ddc653c6fe08bc0f33138fb9b89780d598958
hussein649 <husseinhebo@gmail.com>
2024-01-27 09:42:55 +0100

    add header and footer

--- MyNetflix/src/App.js
+++ MyNetflix/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
-
+import Home from './Pages/Home/Home';
 function App() {
   return (
-    <>
-    <h1>Hello world</h1></>
+    <div className='App'>
+   <Home/>
+    </div>
 
   );
 }
